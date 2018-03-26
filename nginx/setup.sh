bf96f931b50c4c868731465ecc8aca95615fb65e
Alex Ware <alex.ware@guardian.co.uk>
2018-03-26 17:31:15 +0100

    initial commit

---
+++ nginx/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+NGINX_HOME=$(nginx -V 2>&1 | grep 'configure arguments:' | sed 's#.*conf-path=\([^ ]*\)/nginx\.conf.*#\1#g')
+
+echo this script needs root access to configure nginx, please enter your sudo password if prompted
+sudo mkdir -p $NGINX_HOME/sites-enabled
+sudo ln -fs $DIR/sf-oauth-test.conf $NGINX_HOME/sites-enabled/sf-oauth-test.conf
+
